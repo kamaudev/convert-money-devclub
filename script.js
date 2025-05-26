ad26dc74dfbc615d5729f6dd11fd4a020fc9aaab
Bowie <santoscsrita@gmail.com>
2025-05-26 14:23:25 -0300

    Formatando número de entrada com Intl

--- script.js
+++ script.js
@@ -5,8 +5,19 @@ const convertButton = document.querySelector(".convert-button")
 //Função do botão//
 function convertValues(){
     const inputCurrencyValues = document.querySelector("#input-currency").value 
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
+    const currencyValueConverted = document.querySelector(".currency-value")
      const dolarToday = 5.69
      const convertedValue = inputCurrencyValues / dolarToday
+
+     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+     }).format(inputCurrencyValues)
+
+     currencyValueConverted.innerHTML = convertedValue
+
+
 }
 
 //Ouvinte de eventos do botão//
